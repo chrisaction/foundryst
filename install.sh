7953f325b0382571e8c87d29deef95ca038b5354
chrisaction <70870320+chrisaction@users.noreply.github.com>
2020-09-10 17:45:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,17 +18,17 @@ echo '<body>'>>index.php
 echo '</html>'>>index.php
 
 wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip -d v2ray1 v2ray-linux-64.zip
-cd v2ray1
+unzip -d vex1 v2ray-linux-64.zip
+cd vex1
 chmod 777 *
 cd ..
 rm -rf v2ray-linux-64.zip
-mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ray
-mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
-rm -rf $HOME/cloudfoundry/v2ray1
+mv $HOME/cloudfoundry/vex1/v2ray $HOME/cloudfoundry/v2ray
+mv $HOME/cloudfoundry/vex1/v2ctl $HOME/cloudfoundry/v2ctl
+rm -rf $HOME/cloudfoundry/vex1
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":4}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":3}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
 echo '  command: '/app/htdocs/v2ray'' >>manifest.yml
@@ -38,7 +38,7 @@ echo '  memory: '$ramsize'M'>>manifest.yml
 ibmcloud target --cf
 ibmcloud cf push
 domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
-vmess=`echo '{"add":"'$domain'","aid":"4","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
+vmess=`echo '{"add":"'$domain'","aid":"3","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 cd ..
 echo 容器已经成功启动
 echo 地址: $domain
